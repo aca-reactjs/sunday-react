07f9de2b69ef8dc1c4f6c265a65209badc431343
sargis <abovyan.sargis90@gmail.com>
2022-02-27 13:51:49 +0400

    connect to local storage

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useState } from "react";
+import { useEffect, useState } from "react";
 import TodoForm from "./components/TodoForm/TodoForm";
 import getId from "./util/generate.util";
 import TodoList from "./components/TodoList/TodoList";
@@ -7,7 +7,9 @@ import "./App.css";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
 
   const handleFormInputChange = (e) => {
     setInputValue(e.target.value);
@@ -29,8 +31,12 @@ function App() {
   const handleListItemClick = (id) => () => {
     const editedList = todoList.map((item) =>
       item.id === id
-        ? { ...item, isEditMode: true }
-        : { ...item, isEditMode: false }
+        ? { ...item, isEditMode: true, draftValue: item.text }
+        : {
+            ...item,
+            isEditMode: false,
+            text: !item.text.trim() ? item.draftValue : item.text,
+          }
     );
 
     setTodoList(editedList);
@@ -48,11 +54,35 @@ function App() {
     e.preventDefault();
 
     const editedList = todoList.map((item) =>
-      item.id === id ? { ...item, isEditMode: false } : item
+      item.id === id
+        ? {
+            ...item,
+            isEditMode: false,
+            text: !item.text.trim() ? item.draftValue : item.text,
+          }
+        : item
     );
 
     setTodoList(editedList);
   };
+
+  const handleListItemDelete = (id) => (e) => {
+    e.stopPropagation();
+
+    setTodoList((prevState) => prevState.filter((item) => item.id !== id));
+  };
+
+  useEffect(() => {
+    const unload = () => {
+      localStorage.setItem("list", JSON.stringify(todoList));
+    };
+
+    window.addEventListener("beforeunload", unload);
+
+    return () => {
+      window.removeEventListener("beforeunload", unload);
+    };
+  }, [todoList]);
 
   return (
     <div className="app">
@@ -66,6 +96,7 @@ function App() {
         onListItemClick={handleListItemClick}
         onListItemChange={handleListItemChange}
         onListItemFormSubmit={handleListItemFormSubmit}
+        onListItemDelete={handleListItemDelete}
       />
     </div>
   );
--- vite/src/components/TodoList/TodoList.jsx
+++ vite/src/components/TodoList/TodoList.jsx
@@ -5,19 +5,33 @@ function TodoList({
   onListItemClick,
   onListItemChange,
   onListItemFormSubmit,
+  onListItemDelete,
 }) {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
-      <h2>Todo list</h2>
+      <h2 className="todoList-header">Todo list</h2>
       <ul className="todoList">
         {todoList.map(({ id, text, isEditMode }) => (
           <li onClick={onListItemClick(id)} className="todoList-item" key={id}>
             {isEditMode ? (
               <form onSubmit={onListItemFormSubmit(id)}>
-                <input value={text} onChange={onListItemChange(id)} />
+                <input
+                  onBlur={onListItemFormSubmit(id)}
+                  className="todoListItem-input"
+                  value={text}
+                  onChange={onListItemChange(id)}
+                />
               </form>
             ) : (
-              <span>{text}</span>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <span>{text}</span>
+                <span
+                  className="todoItem-delete"
+                  onClick={onListItemDelete(id)}
+                >
+                  X
+                </span>
+              </div>
             )}
           </li>
         ))}
